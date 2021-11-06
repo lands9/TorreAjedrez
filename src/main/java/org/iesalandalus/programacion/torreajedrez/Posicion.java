51c925902bb763b3229ff806a7c277facf34ca77
danig <danigg234@gmail.com>
2021-11-06 12:20:21 +0100

    Creamos los metodos get y set para la clase Posición y establecemos las excepciones en los setters para controlar errores no controlados por java

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -3,4 +3,24 @@ package org.iesalandalus.programacion.torreajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
+	
+	public int getFila() {
+		return fila;
+	}
+	private void setFila(int fila) {
+		if (fila <=0 || fila>=9) {
+			throw new IllegalArgumentException("Error: La fila no es correcta");
+		}
+		this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	private void setColumna(char columna) {
+		if (columna!='a' || columna!='b' || columna!='c' || columna!='d' || columna!='e' || columna!='f' || columna!='g' || columna!='h') {
+			throw new IllegalArgumentException("Error: La columna no es correcta");
+		}
+		this.columna = columna;
+	}
+	
 }
