289f548d51bfa75b6e7a3dfab1dc97533ce49eba
danig <danigg234@gmail.com>
2021-11-09 12:49:04 +0100

    Crear los metodos getter an setter con la excepción correspondiente.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -4,5 +4,28 @@ public class Torre {
 	private Color color;
 	private Posicion posicion;
 	
+	public Color getColor() {
+		return color;
+	}
+	private void setColor(Color color) {
+		if (color == null || color.equals("")) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		}
+		if(!color.equals("BLANCO")  || !color.equals("NEGRO")) {
+			throw new IllegalArgumentException("El color no es el esperado.");
+		}
+		this.color = color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setPosicion(Posicion posicion) {
+		if(posicion == null) {
+			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");
+		}
+		
+		this.posicion = posicion;
+	}
+	
 	
 }
