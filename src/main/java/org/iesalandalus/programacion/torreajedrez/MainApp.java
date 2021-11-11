ec98437c78e75573872d899303b5061de26ce0fb
danig <danigg234@gmail.com>
2021-11-11 21:50:39 +0100

    Creamos el metodo mostrar Torre (mostrarTorre())

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,9 +1,25 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class MainApp {
-
+private static Torre torre;
 	public static void main(String[] args) {
 		
+		
+		
+		
 	}
+	
+	private static void mostrarTorre() {
+		
+		try {
+			System.out.println(torre.toString());
+			
+		}catch(NullPointerException e){
+			System.out.println("ERROR: La torre aún no está creada.");
+		}
+
+	}
+	
+	
 
 }
