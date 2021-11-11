48eed7ad52d7e08d0546656fa934d4b4b00e1e07
danig <danigg234@gmail.com>
2021-11-11 21:55:14 +0100

    Creamos el metodo mostrarMenu() con las opciones a elegir.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -20,6 +20,13 @@ private static Torre torre;
 
 	}
 	
-	
+	private static void mostrarMenu() {
+		System.out.println("1. Crear una torre por defecto.");
+		System.out.println("2. Crear una torre por defecto con el color a elegir.");
+		System.out.println("3. Crear una torre por con el color a elegir y su posición.");
+		System.out.println("4. Mover o enrocar la torre.");
+		System.out.println("5. salir");
+		
+	}
 
 }
